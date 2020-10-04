2d7625353b5afe2e4943d329f5be98f3dda7cdab
Diksha220 <60913047+Diksha220@users.noreply.github.com>
2020-10-04 12:20:14 +0530

    movile-nav

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 var ham = document.getElementById('ham');
+var x = document.getElementById('x');
 ham.addEventListener('click', () =>{
-    
-})
+    ham.classList.add("hidden");
+    x.classList.remove("hidden");
+    document.querySelector(".mobile-nav").classList.remove('hidden');
+    console.log('a');
+});
+
+x.addEventListener('click', () =>{
+    x.classList.add("hidden");
+    ham.classList.remove("hidden");
+    document.querySelector(".mobile-nav").classList.add('hidden');
+    console.log('a');
+});
